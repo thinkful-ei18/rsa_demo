07ba2e878ee6239ca1246b72b00b80a5b6155db8
OD-003 <od-003@od006deMBP.local>
2018-07-10 11:14:47 -0700

    hot fix procedure change

--- routers/rsa.route.js
+++ routers/rsa.route.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const Magic = require('../lib/magic_num')
+const MagicNumber = new Magic()
+
 const {fetchKeyAsync,generateKeys,removeFileAsync} = require('../lib/key-gen')
 router.get('/generateMagicNumber',async(req,res,next)=> {
     try{
-        const magicNum = await generateKeys()
+        const magicNum = await MagicNumber.generateMagicNumberAsync('sha256',16)
         res.status(200).json({magic_token:magicNum})
     }catch(err){
         return next(err)
@@ -14,6 +17,7 @@ router.post('/access-public',async(req,res,next)=> {
     try{
         const {magicNum} = req.body
         if(!magicNum) throw new Error('access denied')
+        await generateKeys(magicNum)
         const publicKey = (await fetchKeyAsync('public', magicNum)).toString()
         res.status(200).json({magic_token:magicNum, publicKey})
         
@@ -32,7 +36,6 @@ router.post('/access-private',async(req,res,next)=> {
         const privateKey = (await fetchKeyAsync('private', magicNum)).toString()
         //delete key-pairs from server
         await Promise.all([removeFileAsync(`${magicNum}.private`),removeFileAsync(`${magicNum}.public`)])
-        console.log('keypairs delete success')
         res.status(200).json({privateKey})
         
     }catch(err){
